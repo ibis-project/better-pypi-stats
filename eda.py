a8f74a9adf325c5de426950f4a871c42b9bd84ce
Cody <cody@dkdc.dev>
2024-05-05 14:09:22 -0400

    updates

---
+++ eda.py
@@ -0,0 +1,45 @@
+# imports
+import re
+import os
+import sys
+import ibis
+
+import logging as log
+import plotly.io as pio
+import ibis.selectors as s
+import plotly.express as px
+
+from rich import print
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, date
+
+# configuration
+## logger
+log.basicConfig(level=log.INFO)
+
+## ibis config
+ibis.options.interactive = True
+ibis.options.repr.interactive.max_rows = 20
+ibis.options.repr.interactive.max_columns = None
+
+# variables
+NOW = datetime.now()
+NOW_7 = NOW - timedelta(days=7)
+NOW_30 = NOW - timedelta(days=30)
+NOW_90 = NOW - timedelta(days=90)
+NOW_180 = NOW - timedelta(days=180)
+NOW_365 = NOW - timedelta(days=365)
+NOW_10 = NOW - timedelta(days=3650)
+
+# connect to database
+host = "clickpy-clickhouse.clickhouse.com"
+port = 443
+user = "play"
+database = "pypi"
+
+con = ibis.clickhouse.connect(
+    host=host,
+    port=port,
+    user=user,
+    database=database,
+)
